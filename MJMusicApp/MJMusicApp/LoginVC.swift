c84aa2e7193c38a19fba96b0f7372a3bd1dd9155
starrpatty28 <starrpatty28@gmail.com>
2017-03-23 22:46:04 -0700

    create an IBOutlet for the username TxtBox to be able to hit enter and the keyboard goes away

--- MJMusicApp/MJMusicApp/LoginVC.swift
+++ MJMusicApp/MJMusicApp/LoginVC.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var userNameTxtFld: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        userNameTxtFld.delegate = self
+        userNameTxtFld.tag = 0
+    }
+    
+    func textFieldShouldReturn(_ userNameTxtField: UITextField) -> Bool
+    {
+        // Try to find next responder
+        if let nextField = userNameTxtField.superview?.viewWithTag(userNameTxtField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            // Not found, so remove keyboard.
+            userNameTxtField.resignFirstResponder()
+        }
+        // Do not add a line break
+        return false
     }
 
     override func didReceiveMemoryWarning() {
