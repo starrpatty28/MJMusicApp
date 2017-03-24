6661150409a9c592892b54c8c66227b82c079d5d
starrpatty28 <starrpatty28@gmail.com>
2017-03-24 14:43:29 -0700

    add a IBAction button from the 'Tap to play' button on top of the Off The Wall Image

--- MJMusicApp/MJMusicApp/OffTheWallAlbumVC.swift
+++ MJMusicApp/MJMusicApp/OffTheWallAlbumVC.swift
@@ -13,7 +13,6 @@ class OffTheWallAlbumVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func backBtnPressed(_ sender: Any) {
@@ -35,15 +34,8 @@ class OffTheWallAlbumVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func playMusic(_ sender: Any) {
     }
-    */
+    
 
 }
