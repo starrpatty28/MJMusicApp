86a2366d5494174777e6d222e4c2690fc389ea43
starrpatty28 <starrpatty28@gmail.com>
2017-03-25 20:39:07 -0700

    add tap to preview button

--- MJMusicApp/MJMusicApp/Invincible AlbumVC.swift
+++ MJMusicApp/MJMusicApp/Invincible AlbumVC.swift
@@ -15,21 +15,25 @@ class Invincible_AlbumVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func youtubeBtnClkd(_ sender: Any) {
+        openURL(url: "https://www.youtube.com/watch?v=7hDon6okRn4&list=PLJNbijG2M7OwRVl3OHUvBRlCOk0dgxXeR")
+    }
+    
+    func openURL(url:String!) {
+        if let url = NSURL(string:url) {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
