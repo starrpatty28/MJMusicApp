9673a2acc98cb92db3e0d623a2e10d76272e7b50
starrpatty28 <starrpatty28@gmail.com>
2017-03-25 17:24:57 -0700

    re-add the youtube link to MJ Dangerous album

--- MJMusicApp/MJMusicApp/Dangerous AlbumVC.swift
+++ MJMusicApp/MJMusicApp/Dangerous AlbumVC.swift
@@ -17,8 +17,14 @@ class Dangerous_AlbumVC: UIViewController {
     }
     
     @IBAction func youtubeBtnClkd(_ sender: Any) {
+        openURL(url: "https://www.youtube.com/watch?v=8IQS_8y-si0&index=4&list=PL15ty5GYCv5u0_w7pusvZtUP225slLmRT")
     }
     
+    func openURL(url:String!) {
+        if let url = NSURL(string:url) {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
     
     @IBAction func backBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
