9eeb86ea8084371e9748d967931b1a9053d5a9c8
starrpatty28 <starrpatty28@gmail.com>
2017-03-25 18:43:20 -0700

    IBAction button to link to youtube, add code to be able to link to youtube

--- MJMusicApp/MJMusicApp/History AlbumVC.swift
+++ MJMusicApp/MJMusicApp/History AlbumVC.swift
@@ -15,6 +15,16 @@ class History_AlbumVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func youtubeBtnClkd(_ sender: Any) {
+        openURL(url: "https://www.youtube.com/watch?v=8k1yj46xH24&list=PL15ty5GYCv5uDdbqMZNPVLeis1My2YV6O")
+    }
+    
+    func openURL(url:String!) {
+        if let url = NSURL(string:url) {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
 
     @IBAction func backBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
